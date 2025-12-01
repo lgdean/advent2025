7956a378d302d8d10757dababf9b93d452e9c62f
L Dean <lgdean@users.noreply.github.com>
2025-11-30 20:36:38 -0800

    add failing test for Day 1
    A good way to discover the need for package.yaml!

---
+++ haskell-aoc2025/src/Day01.hs
@@ -0,0 +1,13 @@
+module Day01
+    (
+      doPart1,
+      doPart2
+    ) where
+
+import Data.Char (isDigit)
+
+doPart1 :: [Char] -> Int
+doPart1 input = 0
+
+doPart2 :: [Char] -> Int
+doPart2 input = 0
---
+++ haskell-aoc2025/test/Day01Spec.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Day01Spec (spec) where
+
+import Test.Hspec
+
+import Day01
+
+spec :: Spec
+spec = do
+  describe "Part 1" $ do
+    it "can handle given example" $ do
+      input <- readFile "inputs/day01-example"
+      doPart1 input `shouldBe` 0
+
+--    it "can solve Part 1" $ do
+--      input <- readFile "inputs/day01"
+--      doPart1 input `shouldBe` 0
+
+--  describe "Part 2" $ do
+--    it "can handle given example" $ do
+--      input <- readFile "inputs/day01-example"
+--      doPart2 input `shouldBe` 0
+--
+--    it "can solve Part 2" $ do
+--      input <- readFile "inputs/day01"
+--      doPart2 input `shouldBe` 0
