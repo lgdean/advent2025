67f2fd0f2f9409d7445b421c75ccdc7e68ca25d6
L Dean <lgdean@users.noreply.github.com>
2025-11-30 21:28:25 -0800

    Day 01, Part 1

--- haskell-aoc2025/src/Day01.hs
+++ haskell-aoc2025/src/Day01.hs
@@ -4,10 +4,24 @@ module Day01
       doPart2
     ) where
 
-import Data.Char (isDigit)
+import Debug.Trace (trace)
 
 doPart1 :: [Char] -> Int
-doPart1 input = 0
+doPart1 input =
+  let rotations = map parseLine $ lines input
+      initPosition = 50
+      positions = applyRotations initPosition (map fst rotations)
+  in trace (show positions) $ length $ filter (==0) positions
+
+applyRotations :: Int -> [Int -> Int] -> [Int]
+applyRotations pos [] = [pos]
+applyRotations pos (r:rs) =
+  pos : applyRotations ((r pos) `mod` 100) rs
+
+parseLine :: String -> (Int -> Int, Int)
+parseLine ('L' : rest) = (((flip (-)) (read rest)), read rest)
+parseLine ('R' : rest) = (((flip (+)) (read rest)), read rest)
+parseLine anythingElse = error ("cannot parse line: " ++ anythingElse)
 
 doPart2 :: [Char] -> Int
 doPart2 input = 0
--- haskell-aoc2025/test/Day01Spec.hs
+++ haskell-aoc2025/test/Day01Spec.hs
@@ -10,11 +10,11 @@ spec = do
   describe "Part 1" $ do
     it "can handle given example" $ do
       input <- readFile "inputs/day01-example"
-      doPart1 input `shouldBe` 0
+      doPart1 input `shouldBe` 3
 
---    it "can solve Part 1" $ do
---      input <- readFile "inputs/day01"
---      doPart1 input `shouldBe` 0
+    it "can solve Part 1" $ do
+      input <- readFile "inputs/day01"
+      doPart1 input `shouldBe` 1172
 
 --  describe "Part 2" $ do
 --    it "can handle given example" $ do
