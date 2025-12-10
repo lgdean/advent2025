400d8627758f36144711e8a0240f26dd8fac0c89
L Dean <lgdean@users.noreply.github.com>
2025-12-09 22:21:17 -0800

    solve Day 10, Part 1

--- haskell-aoc2025/src/Day10.hs
+++ haskell-aoc2025/src/Day10.hs
@@ -1,26 +1,46 @@
 module Day10
     (
+      fewestButtonPresses,
       doPart1,
 --      doPart2
     ) where
 
 import Data.List (nub, sort)
 import Data.List.Split (splitOn)
-import Data.Set (Set)
-import qualified Data.Set as Set
 
 import Debug.Trace (trace)
 
-type Location = (Int, Int)
+fewestButtonPresses :: [Char] -> [[Int]] -> Int
+fewestButtonPresses pattern buttons =
+  let comboTimes = createCombos (length buttons) [0,1] :: [[Int]]
+      combos = map (\c -> zipWith replicate c buttons) comboTimes :: [[[[Int]]]]
+      buttonPressResult combo = [if odd (length $ filter (==x) (concat $ concat combo)) then '#' else '.' | x <- [0..length pattern-1]]
+      comboResults = trace (show combos) $ map buttonPressResult combos
+      goodCombos = trace (show comboResults) $ filter ((==pattern) . buttonPressResult) combos
+      comboLengths = trace (show goodCombos) $ map (length . concat) goodCombos
+  in minimum comboLengths
+
+-- surely doable with replicateM or the like
+createCombos :: Int -> [a] -> [[a]]
+createCombos 0 _ = [] -- or [[]] ? but unused at the moment anyway
+createCombos 1 xs = [[x] | x <- xs]
+createCombos n xs = [x:rest | x <- xs, rest <- createCombos (n-1) xs]
 
 doPart1 :: [Char] -> Int
 doPart1 input =
-  let allLines = lines input
-  in length allLines
+  let machines = map parseLine $ lines input
+  in sum $ map (uncurry fewestButtonPresses) machines
 
-parse2dCoord :: String -> (Int, Int)
-parse2dCoord cube =
-  let parts = splitOn "," cube
+parseLine :: String -> ([Char], [[Int]])
+parseLine line =
+  let parts = splitOn " " line
   in case parts of
-    [x,y] -> (read x, read y)
-    _       -> error ("cannot parse coordinates: " ++ cube)
+    [] -> error "empty line?"
+    [_] -> error "just one item in line"
+    [_, _] -> error "still not enough"
+    (pattern : rest) -> (reverse $ tail $ reverse $ tail pattern, map parseButton $ tail $ reverse rest)
+
+parseButton :: String -> [Int]
+parseButton input =
+  let parts = splitOn "," $ reverse $ tail $ reverse $ tail input
+  in map read parts
--- haskell-aoc2025/test/Day10Spec.hs
+++ haskell-aoc2025/test/Day10Spec.hs
@@ -8,13 +8,16 @@ import Day10
 spec :: Spec
 spec = do
   describe "Part 1" $ do
+    it "can handle first given machine" $ do
+      fewestButtonPresses ".##." [[3], [1,3], [2], [2,3], [0,2], [0,1]] `shouldBe` 2
+
     it "can handle given example" $ do
       input <- readFile "inputs/day10-example"
       doPart1 input `shouldBe` 7
 
---    it "can solve Part 1" $ do
---      input <- readFile "inputs/day10"
---      doPart1 input `shouldBe` 0
+    it "can solve Part 1" $ do
+      input <- readFile "inputs/day10"
+      doPart1 input `shouldBe` 498
 
 --  describe "Part 2" $ do
 --    it "can handle given example" $ do
