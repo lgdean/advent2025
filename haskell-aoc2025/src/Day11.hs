768341273387c3ec8c13a126a5a66b76dbd010ee
L Dean <lgdean@users.noreply.github.com>
2025-12-11 18:52:05 -0800

    try a thing with a Map and foldl
    Doesn't go all the way to returning the Map, which I could do,
    but I wonder whether another approach could work better.

--- haskell-aoc2025/src/Day11.hs
+++ haskell-aoc2025/src/Day11.hs
@@ -9,6 +9,7 @@ import Lib (strip)
 import Data.List.Split (splitOn)
 import qualified Data.Map.Strict as Map
 import Data.Map (Map)
+import Data.Maybe (mapMaybe)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
@@ -17,14 +18,24 @@ import qualified Data.Set as Set
 doPart1 :: [Char] -> Int
 doPart1 input =
   let deviceMap = Map.fromList $ map parseLine $ lines input
-      paths = pathsFrom deviceMap Set.empty "you"
+      paths = pathsFrom deviceMap Set.empty Map.empty "you"
   in length paths
 
-pathsFrom :: Map String [String] -> Set String -> String -> [[String]]
-pathsFrom _ _ "out" = [["out"]]
-pathsFrom outputMap seenSoFar src
+-- TODO returned Map is incomplete, which may be fine, or may be hack leading to bugs
+--pathsFrom :: Map String [String] -> Set String -> Map String [[String]] -> String -> (Map String [[String]], [[String]])
+pathsFrom :: Map String [String] -> Set String -> Map String [[String]] -> String -> [[String]]
+pathsFrom _ _ _ "out" = [["out"]]
+pathsFrom outputMap seenSoFar knownPathsOut src
   | src `Set.member` seenSoFar = [] -- cycle detected
-  | otherwise = map (src:) $ concatMap (pathsFrom outputMap (Set.insert src seenSoFar)) $ Map.findWithDefault [] src outputMap
+  | src `Map.member` knownPathsOut = knownPathsOut Map.! src
+  | otherwise =
+     let nextSteps = Map.findWithDefault [] src outputMap
+         nextSeenSoFar = Set.insert src seenSoFar
+         pathInfoFrom currMap dev = pathsFrom outputMap nextSeenSoFar currMap dev
+         pathAndMapFrom currMap dev = Map.insert dev (pathInfoFrom currMap dev) currMap
+         newPathMap = foldl' pathAndMapFrom knownPathsOut nextSteps :: Map String [[String]]
+--     in map (src:) $ concatMap (pathInfoFrom knownPathsOut) nextSteps
+     in map (src:) $ concat $ mapMaybe (\d -> Map.lookup d newPathMap) nextSteps
 
 parseLine :: String -> (String, [String])
 parseLine line =
@@ -37,6 +48,6 @@ parseLine line =
 doPart2 :: [Char] -> Int
 doPart2 input =
   let deviceMap = Map.fromList $ map parseLine $ lines input
-      paths = pathsFrom deviceMap Set.empty "svr"
+      paths = pathsFrom deviceMap Set.empty Map.empty "svr"
       relevantPaths = filter (\p -> "dac" `elem` p && "fft" `elem` p) paths
   in length relevantPaths
